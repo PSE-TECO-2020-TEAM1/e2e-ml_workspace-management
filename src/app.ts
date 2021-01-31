d2ca80b41005b9e80fb63db1c564643338641d8f
KtrauM <murat.kurnaz12@gmail.com>
2021-01-31 12:47:52 +0300

    dotenv setup done

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("hello world");
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+dotenv.config();
+
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_IP}/${process.env.DATABASE}?retryWrites=true&w=majority`,
+        {   
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true 
+        });
+
+mongoose.connection.on('error', console.error.bind(console, 'Connection error:'));        
+mongoose.connection.once('open', function() {
+    console.log("Database connection established");
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log("Server started");
+});
